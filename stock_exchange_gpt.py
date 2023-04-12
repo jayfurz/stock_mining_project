92b02e0cb1b27c9a7ac22c8faa4ee73f918c5e32
Justin Fursov <justin.fursov.155@my.csun.edu>
2023-04-12 00:18:31 -0700

    updated the gpt processing. Need to change the way prompts and completions are set up

--- stock_exchange_gpt.py
+++ stock_exchange_gpt.py
@@ -2,52 +2,43 @@ import openai
 import os
 import numpy as np
 import pandas as pd
+import json
 
 # if you are using a notebook, you do not need the following
 import stock_exchange_split as split
 
 
-def format_data_for_gpt(x_train, y_train, n=5):
-    """
-    Format the training data in the required GPT format.
-    """
-    formatted_data = []
+def prepare_finetuning_data(data_obj, output_file, step=5):
+    finetuning_data = []
 
-    for i in range(len(x_train)):
-        # Extract the previous n rows of data
-        prev_data = x_train.iloc[max(0, i - n) : i].to_numpy().tolist()
+    x_train_data = data_obj.x_train.values
+    y_train_data = data_obj.y_train.values
 
-        # Extract the current row's data
-        curr_data = x_train.iloc[i].to_numpy().tolist()
+    for i in range(0, len(x_train_data), step):
+        x_data = x_train_data[i:i + step]
+        y_data = y_train_data[i:i + step]
 
-        # Combine the previous n rows and current row's data
-        input_data = np.array(prev_data + [curr_data]).flatten().tolist()
+        prompt = json.dumps(x_data.tolist())
+        completion = json.dumps(y_data.tolist())
 
-        # Convert the input data to a string
-        input_str = ", ".join([str(x) for x in input_data])
+        finetuning_data.append({"prompt": prompt, "completion": completion})
 
-        # Format the input string for GPT training
-        formatted_str = f"{input_str} - {y_train.iloc[i]:.4f}"
+    with open(output_file, "w") as f:
+        for item in finetuning_data:
+            f.write(json.dumps(item) + "\n")
 
-        formatted_data.append(formatted_str)
-
-    return formatted_data
+prepare_finetuning_data(split.nasdaq_train_test, "nasdaq_finetuning_data.jsonl")
+prepare_finetuning_data(split.nyse_train_test, "nyse_finetuning_data.jsonl")
 
 
-# Format the training data for the GPT model (for without a notebook)
-formatted_train_data = format_data_for_gpt(
-    split.nasdaq_train_test.x_train, split.nasdaq_train_test.y_train
-)
-
-# Upload the formatted training data to the OpenAI API
+# Upload the formatted training data to GPT (not ready)
 model_name = "ada"
 openai.api_key = os.environ["OPENAI_API_KEY"]
-response = openai.Completion.create(
-    engine=model_name,
-    prompt=formatted_train_data,
-    max_tokens=1024,
-    n=1,
-    stop=None,
-    temperature=0.5,
-)
-print(response)
+#openai.FineTune.create(
+#    engine=model_name,
+#    prompt=formatted_train_data,
+#    max_tokens=1024,
+#    n=1,
+#    stop=None,
+#    temperature=0.5,
+#)
