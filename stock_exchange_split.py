40945f762e872a102bdb8048b7d52bdf25f5cc98
jayfurz <justin.fursov.155@my.csun.edu>
2023-04-13 14:58:29 -0700

    Changed the splitting process to be GPT friendly

--- stock_exchange_split.py
+++ stock_exchange_split.py
@@ -1,16 +1,18 @@
 import pandas as pd
-from sklearn.model_selection import train_test_split
+import numpy as np
 
+class DataPreprocessor:
+    def __init__(self, data):
+        self.data = data
 
-class train_test_data:
-    def __init__(self, train_data, test_data):
-        # Separate the features and target variable for the training and testing data
-        self.x_train = train_data.drop(["Percent_Change"], axis=1)
-        self.y_train = train_data["Percent_Change"]
+    def generate_sequences(self, sequence_length):
+        x_data, y_data = [], []
 
-        self.x_test = test_data.drop(["Percent_Change"], axis=1)
-        self.y_test = test_data["Percent_Change"]
+        for i in range(len(self.data) - sequence_length):
+            x_data.append(self.data.iloc[i : i + sequence_length - 1].values)
+            y_data.append(self.data.iloc[i + sequence_length - 1]["Percent_Change"])
 
+        return np.array(x_data), np.array(y_data)
 
 # Load the preprocessed NASDAQ data
 nasdaq_data = pd.read_csv("nasdaq_data.csv")
@@ -29,12 +31,20 @@ nyse_data = nyse_data[
     & (nyse_data["Open"] != nyse_data["Low"])
 ]
 
-# Split the NASDAQ data into training and testing sets
-nasdaq_train, nasdaq_test = train_test_split(nasdaq_data, test_size=0.2)
+# Create DataPreprocessor objects for both datasets
+nasdaq_preprocessor = DataPreprocessor(nasdaq_data)
+nyse_preprocessor = DataPreprocessor(nyse_data)
 
-# Split the NYSE data into training and testing sets
-nyse_train, nyse_test = train_test_split(nyse_data, test_size=0.2)
+# Generate sequences for NASDAQ and NYSE data
+sequence_length = 5
+nasdaq_x, nasdaq_y = nasdaq_preprocessor.generate_sequences(sequence_length)
+nyse_x, nyse_y = nyse_preprocessor.generate_sequences(sequence_length)
 
-# Create objects for both sets to make it cleaner
-nasdaq_train_test = train_test_data(nasdaq_train, nasdaq_test)
-nyse_train_test = train_test_data(nyse_train, nyse_test)
+# Split the data into training and testing sets
+test_size = int(0.2 * len(nasdaq_x))
+nasdaq_x_train, nasdaq_x_test = nasdaq_x[:-test_size], nasdaq_x[-test_size:]
+nasdaq_y_train, nasdaq_y_test = nasdaq_y[:-test_size], nasdaq_y[-test_size:]
+
+test_size = int(0.2 * len(nyse_x))
+nyse_x_train, nyse_x_test = nyse_x[:-test_size], nyse_x[-test_size:]
+nyse_y_train, nyse_y_test = nyse_y[:-test_size], nyse_y[-test_size:]
