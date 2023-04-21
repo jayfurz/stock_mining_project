599562391d588e2cdf6aaad9dcb331a4f8c5e42b
Justin Fursov <justinfursov@justins-mbp.mynetworksettings.com>
2023-04-20 20:59:43 -0700

    added comparison charts

--- predictions/plot_candlesticks.py
+++ predictions/plot_candlesticks.py
@@ -1,3 +1,7 @@
+# This python script was used to plot predicted candlesticks on the modern day IXIC
+# The prompt for this test was 15 days of data, and the response was 10 days of data proceeding
+
+import ast
 import json
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
@@ -6,11 +10,8 @@ import mplfinance as mpf
 import pandas as pd
 import os
 
-with open('ixic_weekly_apr_18_23.json', 'r') as file:
-    data = json.load(file)
-print(data)  
 def plot_candlestick(data, title):
-    df = pd.DataFrame(data, columns=['Date', 'Open', 'High', 'Low', 'Close', 'Adj Close', 'Volume', 'Change', 'ChangeValue', '???'])
+    df = pd.DataFrame(data, columns=['Date', 'Open', 'High', 'Low', 'Close', 'Adj Close', 'Volume', 'Change', 'ChangeValue'])
     df['Date'] = pd.to_datetime(df['Date'])
     df.set_index('Date', inplace=True)
 
@@ -20,17 +21,37 @@ def plot_candlestick(data, title):
 
     plt.show()
 
-prompt_data = json.loads(data[0]["prompt"])
-response_data = json.loads(data[0]["response"])
-combined_data = prompt_data + response_data
+def april_18_prediction():
+    with open('ixic_weekly_apr_18_23.json', 'r') as file:
+        data = json.load(file)
+    print(data)  
+    prompt_data = json.loads(data[0]["prompt"])
+    response_data = json.loads(data[0]["response"])
+    combined_data = prompt_data + response_data
 
-index_2023_04_18 = [i for i, x in enumerate(combined_data) if x[0] == "2023-04-18"][0]
+    index_2023_04_18 = [i for i, x in enumerate(combined_data) if x[0] == "2023-04-18"][0]
 
-# Add a dotted line between 2023-04-18 and 2023-04-19
-for i, row in enumerate(combined_data):
-    if i == index_2023_04_18:
-        combined_data[i].append(True)
-    else:
-        combined_data[i].append(False)
+    # Add a dotted line between 2023-04-18 and 2023-04-19
+    for i, row in enumerate(combined_data):
+        if i == index_2023_04_18:
+            combined_data[i].append(True)
+        else:
+            combined_data[i].append(False)
 
-plot_candlestick(combined_data, "Stock Data with Dotted Line and Gridlines")
+    plot_candlestick(combined_data, "Stock Data Prediction on the IXIC from April 18")
+
+def evaluation_of_test_graph():
+    with open('../dump/evaluation_results_20230418_134035.json') as file:
+        data = json.load(file)
+
+    response_data = []
+    actual_data = []
+    for datum in data:
+        response_data.append(ast.literal_eval(datum["response"]))
+        actual_data.append(ast.literal_eval(datum["actual"]))
+
+    plot_candlestick(response_data, "Model's predicted outcomes from 2016-2021")
+    plot_candlestick(actual_data, "Actual outcomes from 2016-2021")
+    
+evaluation_of_test_graph()
+
