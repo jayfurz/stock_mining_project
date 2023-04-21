6e9488c83b50f8bd2d0bc93156a67c6dfff825be
Justin Fursov <justinfursov@justins-mbp.mynetworksettings.com>
2023-04-20 22:37:35 -0700

    added some more graphing python things.

--- model_evaluation.py
+++ model_evaluation.py
@@ -1,6 +1,8 @@
 import json
 from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
 
 # Load data from the JSON file
 data_file = "./dump/evaluation_results_20230418_134035.json"
@@ -34,9 +36,50 @@ def check_direction(actual_values, predicted_values):
             correct_direction += 1
     return correct_direction
 
+# Function to calculate Mean Absolute Percentage Error
+def mean_absolute_percentage_error(y_true, y_pred):
+    y_true, y_pred = np.array(y_true), np.array(y_pred)
+    return np.mean(np.abs((y_true - y_pred) / y_true)) * 100
+
+def calculate_changes(predicted_values):
+    changes = []
+    for row in predicted_values:
+        open_price = row[1]
+        close_price = row[4]
+        absolute_change = close_price - open_price
+        percent_change = absolute_change / open_price 
+        changes.append((percent_change, absolute_change))
+    return changes
+
+def check_changes(actual_values, predicted_values):
+    correct_percent_changes = 0
+    correct_absolute_changes = 0
+    total = len(actual_values)
+
+    predicted_changes = calculate_changes(predicted_values)
+
+    for i in range(total):
+        actual_percent_change = actual_values[i][-2]
+        actual_absolute_change = actual_values[i][-1]
+
+        predicted_percent_change = predicted_changes[i][0]
+        predicted_absolute_change = predicted_changes[i][1]
+
+        if round(predicted_values[i][-2], 3) == round(predicted_percent_change, 3):
+            correct_percent_changes += 1
+
+        if round(predicted_values[i][-1], 1) == round(predicted_absolute_change, 1):
+            correct_absolute_changes += 1
+
+    return correct_percent_changes, correct_absolute_changes, total
+
+predicted_changes = calculate_changes(predicted_values)
+
+for i in range(len(predicted_values)):
+    predicted_values[i].extend(predicted_changes[i][:-2])
 # Calculate regression metrics for each value (open, high, low, close, and volume)
 metrics = {}
-for i, name in enumerate(["Open", "High", "Low", "Close", "Volume"]):
+for i, name in enumerate(["Open", "High", "Low", "Close", "AdjClose", "Volume", "PercentChange", "AbsChange"]):
     actual_col = [row[i + 1] for row in actual_values]
     predicted_col = [row[i + 1] for row in predicted_values]
     
@@ -44,8 +87,9 @@ for i, name in enumerate(["Open", "High", "Low", "Close", "Volume"]):
     mse = mean_squared_error(actual_col, predicted_col)
     rmse = np.sqrt(mse)
     r2 = r2_score(actual_col, predicted_col)
+    mape = mean_absolute_percentage_error(actual_col, predicted_col)
 
-    metrics[name] = {"MAE": mae, "MSE": mse, "RMSE": rmse, "R²": r2}
+    metrics[name] = {"MAE": mae, "MSE": mse, "RMSE": rmse, "R²": r2, "MAPE": mape}
 
 for name, values in metrics.items():
     print(f"{name}:")
@@ -58,4 +102,31 @@ correct_direction = check_direction(actual_values, predicted_values)
 
 print(f"Correct Dates: {correct_dates}/{len(actual_values)}")
 print(f"Correct Direction: {correct_direction}/{len(actual_values)}")
+
+correct_percent_changes, correct_absolute_changes, total = check_changes(actual_values, predicted_values)
+
+print(f"Correct percent changes: {correct_percent_changes}/{total} ({(correct_percent_changes / total) * 100:.2f}%)")
+print(f"Correct absolute changes: {correct_absolute_changes}/{total} ({(correct_absolute_changes / total) * 100:.2f}%)")
+
+actual_close_prices = [actual[3] for actual in actual_values]
+predicted_close_prices = [predicted[3] for predicted in predicted_values]
+actual_daily_returns = pd.Series(actual_close_prices).pct_change()
+predicted_daily_returns = pd.Series(predicted_close_prices).pct_change()
+actual_cumulative_returns = (1 + actual_daily_returns).cumprod() - 1
+predicted_cumulative_returns = (1 + predicted_daily_returns).cumprod() - 1
+annual_risk_free_rate = 0.03
+daily_risk_free_rate = (1 + annual_risk_free_rate) ** (1 / 252) - 1
+actual_excess_returns = actual_daily_returns - daily_risk_free_rate
+predicted_excess_returns = predicted_daily_returns - daily_risk_free_rate
+actual_sharpe_ratio = np.mean(actual_excess_returns) / np.std(actual_excess_returns)
+predicted_sharpe_ratio = np.mean(predicted_excess_returns) / np.std(predicted_excess_returns)
+print(f"Actual sharpe ratio: {actual_sharpe_ratio}")
+print(f"Predicted sharpe ratio: {predicted_sharpe_ratio}")
+
+plt.plot(actual_cumulative_returns, label="Actual Cumulative Returns")
+plt.plot(predicted_cumulative_returns, label="Predicted Cumulative Returns")
+plt.xlabel("Days")
+plt.ylabel("Cumulative Returns")
+plt.legend()
+plt.show()
 
--- predictions/plot_candlesticks.py
+++ predictions/plot_candlesticks.py
@@ -9,6 +9,41 @@ from datetime import datetime
 import mplfinance as mpf
 import pandas as pd
 import os
+import plotly.graph_objs as go
+from plotly.subplots import make_subplots
+
+def plot_candlestick_interactive(response_data, actual_data, title):
+    response_df = pd.DataFrame(response_data, columns=['Date', 'Open', 'High', 'Low', 'Close', 'Adj Close', 'Volume', 'Change', 'ChangeValue'])
+    response_df['Date'] = pd.to_datetime(response_df['Date'])
+    
+    actual_df = pd.DataFrame(actual_data, columns=['Date', 'Open', 'High', 'Low', 'Close', 'Adj Close', 'Volume', 'Change', 'ChangeValue'])
+    actual_df['Date'] = pd.to_datetime(actual_df['Date'])
+    
+    fig = make_subplots(specs=[[{"secondary_y": True}]])
+    
+    fig.add_trace(go.Candlestick(x=response_df['Date'],
+                    open=response_df['Open'],
+                    high=response_df['High'],
+                    low=response_df['Low'],
+                    close=response_df['Close'],
+                    name="Predicted",
+                    increasing_line_color='green',
+                    decreasing_line_color='red'),
+                    secondary_y=False)
+
+    fig.add_trace(go.Candlestick(x=actual_df['Date'],
+                    open=actual_df['Open'],
+                    high=actual_df['High'],
+                    low=actual_df['Low'],
+                    close=actual_df['Close'],
+                    name="Actual",
+                    increasing_line_color='green',
+                    decreasing_line_color='red',
+                    visible="legendonly"),
+                    secondary_y=False)
+    
+    fig.update_layout(title=title, xaxis_rangeslider_visible=False, xaxis_title="Date", yaxis_title="Price")
+    fig.show()
 
 def plot_candlestick(data, title):
     df = pd.DataFrame(data, columns=['Date', 'Open', 'High', 'Low', 'Close', 'Adj Close', 'Volume', 'Change', 'ChangeValue'])
@@ -49,7 +84,7 @@ def evaluation_of_test_graph():
     for datum in data:
         response_data.append(ast.literal_eval(datum["response"]))
         actual_data.append(ast.literal_eval(datum["actual"]))
-
+    plot_candlestick_interactive(response_data, actual_data, "Model's predicted outcomes vs Actual outcomes from 2016-2021")
     plot_candlestick(response_data, "Model's predicted outcomes from 2016-2021")
     plot_candlestick(actual_data, "Actual outcomes from 2016-2021")
     
